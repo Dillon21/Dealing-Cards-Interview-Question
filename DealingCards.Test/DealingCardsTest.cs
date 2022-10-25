1c6d79b4939f073b24f63312c8f49fc164361714
Dillon Scott <dillon.scott@codify.ltd.uk>
2022-10-25 15:38:05 +0100

    Working sectioning deal

--- DealingCards.Test/DealingCardsTest.cs
+++ DealingCards.Test/DealingCardsTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Security.Cryptography;
 
 namespace DealingCards.Tests{
 
@@ -13,7 +14,36 @@ namespace DealingCards.Tests{
         }
 
         [Test]
-        public void
+        public void AddPlayers_GetPlayersFromInput_ReturnPlayers(int input, int ExpectedResult){
+            ;
+        }
+
+        [Test]
+        public void AddPlayers_GetPlayersFromInput_ReturnException(){
+
+        }
+
+        public List<int> AddPlayers(int players) {
+            var playerList = new List<int>();
+            do {
+                Console.WriteLine("How many players?");
+                playerList = Enumerable.Range(1,players).ToList();
+                if (playerList.Count > 8 | playerList.Count == 0) {
+                    throw new Exception("Player count must be between 1 and 8");
+                }
+            } while (playerList.Count > 8 | playerList.Count == 0);
+
+            return playerList;
+        }
+
+        [Test]
+        public void ReadTxt_GetTextLineByLine_ReturnTextInTxtFile() {
+            var line = File.ReadAllLines("cards.txt");
+
+            Assert.NotNull(line);
+            Assert.Greater(line.Length, 0);
+
+        }
 
     }
 }
--- DealingCards/Deal.cs
+++ DealingCards/Deal.cs
@@ -9,30 +9,62 @@ namespace DealingCards {
 
     public class Deal{
 
+        List<int> playerList;
+        List<Card> cards = new List<Card>();
+        Dictionary<int, List<Card>> playerCardPairs = new Dictionary<int, List<Card>>();
+
+
+
         public Dictionary<int, List<Card>> DealCards(){
-
-        
-            Console.WriteLine("How many players?");
-            //string playerCount = Console.ReadLine();
-
-            List<Card> cards = new List<Card>();
-            Dictionary<int, List<Card>> playerCardPairs = new Dictionary<int, List<Card>>();
+            
+            AddPlayers();
 
             foreach (var line in File.ReadAllLines("cards.txt")){
                 if(!line.Equals("Joker")){
                 var values = line.Split(' ');
                 cards.Add(new Card(values[0], values[1]));
+                }
             }
 
+            Random rand = new Random();
+            var shuffledCards = cards.OrderBy(_ => rand.Next()).ToList(); 
+
+            playerList.ForEach(p => {
+                playerCardPairs.Add(p, shuffledCards
+                               .Take(new Range(((shuffledCards.Count / playerList.Count) * (p-1)),((shuffledCards.Count / playerList.Count) * p)))
+                               .ToList());
+            });
+
+            foreach(var key in playerCardPairs.Keys){
+                Console.WriteLine("\nCards in player " + key + " Hand\nHand Size: " + playerCardPairs[key].Count);
+
+                foreach (var card in playerCardPairs[key]){
+                    Console.WriteLine(card.Value + " " + card.Suit);
+                    cards.Remove(card);
+                }
             }
 
-            
+            if(cards.Count > 0){
+            Console.WriteLine("\nLeft over cards");
+            printPlayerCards(0, cards);
+            }
 
             return playerCardPairs;
 
         }
 
 
+        public List<int> AddPlayers(){
+            do {
+                Console.WriteLine("How many players?");
+                playerList = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToList();
+                if (playerList.Count > 8 | playerList.Count == 0) {
+                    Console.WriteLine("Player count must be between 1 and 8");
+                }
+            } while (playerList.Count > 8 | playerList.Count == 0);
+
+            return playerList;
+        }
 
             
         public void printPlayerCards(int player, List<Card> cards){
@@ -43,8 +75,15 @@ namespace DealingCards {
 
         public static void Main(string[] args) {
             Deal deal = new Deal();
-            deal.DealCards();
+
+            try{
+                deal.DealCards();
+            }catch (Exception e){
+                Console.WriteLine(e.Message);
+                deal.DealCards();
+            }
+            
         }
     }
+    }
 
-}
