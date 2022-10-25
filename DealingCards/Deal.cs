43e6073dd92f56e619d2ed77e90ca98beb07ef2a
Dillon Scott <dillon.scott@codify.ltd.uk>
2022-10-25 16:13:07 +0100

    Added leftover cards and made more readable

--- DealingCards/Deal.cs
+++ DealingCards/Deal.cs
@@ -15,9 +15,15 @@ namespace DealingCards {
 
 
 
-        public Dictionary<int, List<Card>> DealCards(){
-            
-            AddPlayers();
+        public Dictionary<int, List<Card>> DealCards() {
+
+            do {
+                Console.WriteLine("How many players?");
+                playerList = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToList();
+                if (playerList.Count > 8 | playerList.Count == 0) {
+                    Console.WriteLine("Player count must be between 1 and 8");
+                }
+            } while (playerList.Count > 8 | playerList.Count == 0);
 
             foreach (var line in File.ReadAllLines("cards.txt")){
                 if(!line.Equals("Joker")){
@@ -27,18 +33,21 @@ namespace DealingCards {
             }
 
             Random rand = new Random();
-            var shuffledCards = cards.OrderBy(_ => rand.Next()).ToList(); 
+            var shuffledCards = cards.OrderBy(_ => rand.Next()).ToList();
 
-            playerList.ForEach(p => {
-                playerCardPairs.Add(p, shuffledCards
-                               .Take(new Range(((shuffledCards.Count / playerList.Count) * (p-1)),((shuffledCards.Count / playerList.Count) * p)))
-                               .ToList());
-            });
+            var handSize = cards.Count() / playerList.Count;
 
-            foreach(var key in playerCardPairs.Keys){
+            foreach(var player in playerList){
+                var hand = shuffledCards.Skip(handSize * (player - 1))
+                    .Take(handSize)
+                    .ToList();
+                playerCardPairs.Add(player, hand);  
+            }
+
+            foreach(var key in playerCardPairs.Keys) {
                 Console.WriteLine("\nCards in player " + key + " Hand\nHand Size: " + playerCardPairs[key].Count);
 
-                foreach (var card in playerCardPairs[key]){
+                foreach (var card in playerCardPairs[key]) {
                     Console.WriteLine(card.Value + " " + card.Suit);
                     cards.Remove(card);
                 }
@@ -53,20 +62,6 @@ namespace DealingCards {
 
         }
 
-
-        public List<int> AddPlayers(){
-            do {
-                Console.WriteLine("How many players?");
-                playerList = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToList();
-                if (playerList.Count > 8 | playerList.Count == 0) {
-                    Console.WriteLine("Player count must be between 1 and 8");
-                }
-            } while (playerList.Count > 8 | playerList.Count == 0);
-
-            return playerList;
-        }
-
-            
         public void printPlayerCards(int player, List<Card> cards){
             cards.ForEach(card => {
                 Console.WriteLine(card.Value + " " + card.Suit);
